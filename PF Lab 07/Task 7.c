44bc819271a45620092528b78f47edc6271aa42b
Saimkhurram-24 <k256500@nu.edu.pk>
2026-03-09 20:19:41 +0500

    Task 7.c

---
+++ PF Lab 07/Task 7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[10], i, j;
+
+    printf("Enter 10 integers:\n");
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < i; j++) {
+            if(a[i] == a[j]) {
+                a[i] = -1;
+                break;
+            }
+        }
+    }
+
+    printf("Updated array:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
